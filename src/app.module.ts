69134acdf5ad0c278ff7d2ea26cfe7e78d0d9002
Dionis <dionisenja@gmail.com>
2021-10-22 10:04:47 +0200

    backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Todo } from './todos/entities/todo.entity';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'qwe123',
+      database: 'nestTodo',
+      entities: [Todo],
+      synchronize: true,
+    }),
+    TodosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
